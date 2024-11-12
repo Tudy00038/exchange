ee9e2f239e75163821fa7b9bdef770834d2d4beb
Tudy00038 <72760281+Tudy00038@users.noreply.github.com>
2024-11-12 09:13:04 +0200

    frontend init

--- projects/exchange/smart_contracts/exchange_points/deploy_config.py
+++ projects/exchange/smart_contracts/exchange_points/deploy_config.py
@@ -3,6 +3,12 @@ import logging
 import algokit_utils
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
+from smart_contracts.artifacts.exchange_points.marketplace_smart_contract_client import (
+    CreateApplicationArgs,
+    DeleteApplicationArgs,
+    DeployCreate,
+    Deploy,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -14,11 +20,11 @@ def deploy(
     app_spec: algokit_utils.ApplicationSpecification,
     deployer: algokit_utils.Account,
 ) -> None:
-    from smart_contracts.artifacts.exchange_points.exchange_points_client import (
-        ExchangePointsClient,
+    from smart_contracts.artifacts.exchange_points.marketplace_smart_contract_client import (
+        MarketplaceSmartContractClient,
     )
 
-    app_client = ExchangePointsClient(
+    app_client = MarketplaceSmartContractClient(
         algod_client,
         creator=deployer,
         indexer_client=indexer_client,
@@ -27,6 +33,10 @@ def deploy(
     app_client.deploy(
         on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
         on_update=algokit_utils.OnUpdate.AppendApp,
+        create_args=DeployCreate(
+            args=CreateApplicationArgs(assetId=123, unitaryPrice=456)
+        ),
+        delete_args=Deploy(args=DeleteApplicationArgs()),
     )
     name = "world"
     response = app_client.hello(name=name)
