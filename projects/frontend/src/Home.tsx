1dd43ed2e7ba51c1f1e20fa5bff774fdcbecb01f
Tudy00038 <72760281+Tudy00038@users.noreply.github.com>
2024-11-18 15:54:19 +0200

    Functioning marketplace app

--- projects/frontend/src/Home.tsx
+++ projects/frontend/src/Home.tsx
@@ -1,24 +1,67 @@
 // src/components/Home.tsx
+import * as algokit from '@algorandfoundation/algokit-utils'
 import { useWallet } from '@txnlab/use-wallet'
-import React, { useState } from 'react'
+import algosdk from 'algosdk'
+import React, { useEffect, useState } from 'react'
 import ConnectWallet from './components/ConnectWallet'
-import Transact from './components/Transact'
-
+import MethodCall from './components/MethodCall'
+import { MarketplaceSmartContractClient } from './contracts/MarketplaceSmartContract'
+import { buy, create, deleteApp } from './methods'
+import { getAlgodConfigFromViteEnvironment } from './utils/network/getAlgoClientConfigs'
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
+  algokit.Config.configure({ populateAppCallResources: true })
   const [openWalletModal, setOpenWalletModal] = useState<boolean>(false)
-  const [openDemoModal, setOpenDemoModal] = useState<boolean>(false)
-  const { activeAddress } = useWallet()
+  const [appId, setAppId] = useState<number>(0)
+  //assetId, unitaryPrice, quantity
+  const [assetId, setAssetId] = useState<bigint>(0n)
+  const [unitaryPrice, setUnitaryPrice] = useState<bigint>(0n)
+  const [quantity, setQuantity] = useState<bigint>(0n)
+  const [unitsLeft, setUnitsLeft] = useState<bigint>(0n)
+  const [seller, setSeller] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    dmClient
+      .getGlobalState()
+      .then((globalState) => {
+        setUnitaryPrice(globalState.unitaryPrice?.asBigInt() || 0n)
+        const id = globalState.assetId?.asBigInt() || 0n
+        setAssetId(id),
+          algorand.account.getAssetInformation(algosdk.getApplicationAddress(appId), id).then((info) => {
+            setUnitsLeft(info.balance)
+          })
+      })
+      .catch(() => {
+        setUnitaryPrice(0n)
+        setAssetId(0n)
+      })
+
+    algorand.client.algod
+      .getApplicationByID(appId)
+      .do()
+      .then((response) => {
+        setSeller(response.params.creator)
+      })
+  }, [appId])
+
+  const { activeAddress, signer } = useWallet()
 
   const toggleWalletModal = () => {
     setOpenWalletModal(!openWalletModal)
   }
 
-  const toggleDemoModal = () => {
-    setOpenDemoModal(!openDemoModal)
-  }
-
+  const algodConfig = getAlgodConfigFromViteEnvironment()
+  const algorand = algokit.AlgorandClient.fromConfig({ algodConfig })
+  algorand.setDefaultSigner(signer)
+  const dmClient = new MarketplaceSmartContractClient(
+    {
+      resolveBy: 'id',
+      id: appId,
+      sender: { addr: activeAddress!, signer },
+    },
+    algorand.client.algod,
+  )
   return (
     <div className="hero min-h-screen bg-teal-400">
       <div className="hero-content text-center rounded-lg p-6 max-w-md bg-white mx-auto">
@@ -31,29 +74,86 @@ const Home: React.FC<HomeProps> = () => {
           </p>
 
           <div className="grid">
-            <a
-              data-test-id="getting-started"
-              className="btn btn-primary m-2"
-              target="_blank"
-              href="https://github.com/algorandfoundation/algokit-cli"
-            >
-              Getting started
-            </a>
-
-            <div className="divider" />
             <button data-test-id="connect-wallet" className="btn m-2" onClick={toggleWalletModal}>
               Wallet Connection
             </button>
+            <div className="divider" />
+            <label className="lable">App ID</label>
+            <input
+              type="number"
+              className="input input-bordered"
+              value={appId}
+              onChange={(e) => setAppId(e.currentTarget.valueAsNumber || 0)}
+            />
 
-            {activeAddress && (
-              <button data-test-id="transactions-demo" className="btn m-2" onClick={toggleDemoModal}>
-                Transactions Demo
+            <div className="divider" />
+            {activeAddress && appId === 0 && (
+              <div>
+                <label className="label">Unitary Price</label>
+                <input
+                  type="number"
+                  className="input input-bordered"
+                  value={(unitaryPrice / BigInt(10e6)).toString()}
+                  onChange={(e) => {
+                    setUnitaryPrice(BigInt(e.currentTarget.valueAsNumber || 0) * BigInt(10e6))
+                  }}
+                />
+                <MethodCall
+                  methodFunction={create(algorand, dmClient, assetId, unitaryPrice, 10n, activeAddress!, setAppId)}
+                  text="Create App"
+                />
+              </div>
+            )}
+
+            {appId !== 0 && (
+              <div>
+                <label className="label">Asset ID</label>
+                <input type="text" className="input input-bordered" value={assetId.toString()} readOnly />
+                <label className="label">Units Left</label>
+                <input type="text" className="input input-bordered" value={unitsLeft.toString()} readOnly />
+              </div>
+            )}
+            <div className="divider" />
+            {activeAddress && appId !== 0 && unitsLeft > 0n && (
+              <div>
+                <label className="label">Price per Unit</label>
+                <input type="text" className="input input-bordered" value={(unitaryPrice / BigInt(10e6)).toString()} readOnly />
+                <label className="label">Desired quantity</label>
+                <input
+                  type="number"
+                  className="input input-bordered"
+                  value={quantity.toString()}
+                  onChange={(e) => {
+                    setQuantity(BigInt(e.currentTarget.value))
+                  }}
+                />
+                <MethodCall
+                  text={`Buy ${quantity} for ${(unitaryPrice * BigInt(quantity)) / BigInt(10e6)} Algo`}
+                  methodFunction={buy(
+                    algorand,
+                    dmClient,
+                    activeAddress,
+                    algosdk.getApplicationAddress(appId),
+                    quantity,
+                    unitaryPrice,
+                    setUnitsLeft,
+                  )}
+                />
+              </div>
+            )}
+
+            {appId !== 0 && unitsLeft <= 0n && activeAddress !== seller && (
+              <button disabled className="btn btn-disabled m-2">
+                SOLD OUT!
               </button>
+            )}
+
+            {appId !== 0 && unitsLeft <= 0n && activeAddress === seller && (
+              <MethodCall methodFunction={deleteApp(algorand, dmClient, setAppId)} text="Delete app" />
             )}
           </div>
 
           <ConnectWallet openModal={openWalletModal} closeModal={toggleWalletModal} />
-          <Transact openModal={openDemoModal} setModalState={setOpenDemoModal} />
         </div>
       </div>
     </div>
---
+++ projects/frontend/src/methods.ts
@@ -0,0 +1,72 @@
+import * as algokit from '@algorandfoundation/algokit-utils'
+import { MarketplaceSmartContractClient } from './contracts/MarketplaceSmartContract'
+
+export function create(
+  algorand: algokit.AlgorandClient,
+  dmClient: MarketplaceSmartContractClient,
+  assetBeingSold: bigint,
+  unitaryPrice: bigint,
+  quantity: bigint,
+  sender: string,
+  setAppId: (id: number) => void,
+) {
+  return async () => {
+    let assetId = assetBeingSold
+    if (assetId === 0n) {
+      const assetCreate = await algorand.send.assetCreate({
+        sender,
+        total: quantity,
+      })
+      assetId = BigInt(assetCreate.confirmation.assetIndex!)
+    }
+
+    const createResult = await dmClient.create.createApplication({ assetId, unitaryPrice })
+
+    const mbrPay = await algorand.transactions.payment({
+      sender,
+      receiver: createResult.appAddress,
+      amount: algokit.algos(0.1 + 0.1),
+      extraFee: algokit.algos(0.001),
+    })
+    await dmClient.optInToAsset({ mbrPay })
+    await algorand.send.assetTransfer({
+      sender,
+      assetId,
+      receiver: createResult.appAddress,
+      amount: quantity,
+    })
+
+    setAppId(Number(createResult.appId))
+  }
+}
+
+export function buy(
+  algorand: algokit.AlgorandClient,
+  dmClient: MarketplaceSmartContractClient,
+  sender: string,
+  appAddress: string,
+  quantity: bigint,
+  unitaryPrice: bigint,
+  setUnitsLeft: (units: bigint) => void,
+) {
+  return async () => {
+    const buyerTxn = await algorand.transactions.payment({
+      sender,
+      receiver: appAddress,
+      amount: algokit.microAlgos(Number(quantity * unitaryPrice)),
+      extraFee: algokit.algos(0.001),
+    })
+    await dmClient.buy({ buyerTxn, quantity })
+
+    const state = await dmClient.getGlobalState()
+    const info = await algorand.account.getAssetInformation(appAddress, state.assetId!.asBigInt())
+    setUnitsLeft(info.balance)
+  }
+}
+
+export function deleteApp(algorand: algokit.AlgorandClient, dmClient: MarketplaceSmartContractClient, setAppId: (id: number) => void) {
+  return async () => {
+    await dmClient.delete.deleteApplication({}, { sendParams: { fee: algokit.algos(0.003) } })
+    setAppId(0)
+  }
+}
